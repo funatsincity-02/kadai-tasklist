d5b5f46ee55744f6c55a445bcc535fa6d86050f2
yoshi.kobo <funatsincity-02@yahoo.co.jp>
2020-09-05 09:06:17 +0000

    lesson15 kadai #1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,27 @@
 class TasksController < ApplicationController
 
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:edit, :show, :update, :destroy]
+  
+
   def index
-    @tasks = Task.all
+    #@tasks = Task.all
+    @tasks = current_user.tasks.all
+    @task = Task.new
+    #binding.pry
+    
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = '新規にタスクが登録されました'
-      redirect_to @task
+      redirect_to root_url
     else
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'タスクは登録できませんでした'
-      render :new
+      render 'toppages/index'
     end
   end
 
@@ -21,19 +30,20 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+    #binding.pry
+    #@task = Task.find(params[:id])
   end
 
   def show
-    @task = Task.find(params[:id])
+    #@task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
+    #@task = Task.find(params[:id])
     
     if @task.update(task_params)
       flash[:success] = 'タスクは正常に更新されました'
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = 'タスクは更新できませんでした'
       render :edit
@@ -41,16 +51,31 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
+    #@task = Task.find(params[:id])
     @task.destroy
     
     flash[:success] = 'タスクは正常に削除されました'
-    redirect_to tasks_url
+    redirect_to root_url
     
   end
 
+
+  private
+
   def task_params
+  #binding.pry
     params.require(:task).permit(:content, :status)
   end
+
+
+  def correct_user
+    #binding.pry
+    @task = current_user.tasks.find_by(id: params[:id])
     
+    unless @task
+      redirect_to tasks_url
+    end
+  end
+    
+
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+  
+  #before_action :require_user_logged_in
+
+  
+  
+  #def index
+  #  @users = User.order(id: :desc).page(params[:page]).per(25)
+  #end
+
+  #def show
+    #@user = User.find(params[:id])
+  #end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+   #binding.pry
+   @user = User.new(user_params)
+   #binding.pry
+
+    if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end 
+
+  
+end
