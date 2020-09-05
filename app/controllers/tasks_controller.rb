73c0e73a207489c5853850f0683b85eb7937d89f
yoshi.kobo <funatsincity-02@yahoo.co.jp>
2020-09-05 23:14:44 +0000

    kadai re-entry

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -21,7 +21,8 @@ class TasksController < ApplicationController
     else
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'タスクは登録できませんでした'
-      render 'toppages/index'
+      render 'tasks/index'
+      
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  #get 'sessions/new'
+  #get 'sessions/create'
+  #get 'sessions/destroy'
 
   root to: 'tasks#index'
   
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
   resources :users, only: [:new, :create]
-  #resources :tasks, only: [:show, :edit, :create, :destroy]
   resources :tasks
 
   
